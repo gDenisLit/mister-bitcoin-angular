2e247544966054af3ce084df70f764d2f95545d8
Denis Lit <denislitviak@gmail.com>
2022-08-23 14:04:47 +0300

    setup cli, and styles setup, build basic paging, add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core'
+import { RouterModule, Routes } from '@angular/router'
+import { ContactDetailsPageComponent } from './pages/contact-details-page/contact-details-page.component'
+import { ContactPageComponent } from './pages/contact-page/contact-page.component'
+import { HomePageComponent } from './pages/home-page/home-page.component'
+import { LoginPageComponent } from './pages/login-page/login-page.component'
+import { SignupPageComponent } from './pages/signup-page/signup-page.component'
+import { StatisticPageComponent } from './pages/statistic-page/statistic-page.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path: 'contact', component: ContactPageComponent },
+  { path: 'login', component: LoginPageComponent },
+  { path: 'signup', component: SignupPageComponent },
+  { path: 'statistic', component: StatisticPageComponent },
+  { path: 'details', component: ContactDetailsPageComponent },
+]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,24 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app-root/app.component';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
+import { ContactDetailsPageComponent } from './pages/contact-details-page/contact-details-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { SignupPageComponent } from './pages/signup-page/signup-page.component';
+import { StatisticPageComponent } from './pages/statistic-page/statistic-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactPageComponent,
+    ContactDetailsPageComponent,
+    HomePageComponent,
+    LoginPageComponent,
+    SignupPageComponent,
+    StatisticPageComponent
   ],
   imports: [
     BrowserModule,
